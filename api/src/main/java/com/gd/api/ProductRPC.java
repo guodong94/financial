c1b4cef737f5b25ce7629b21d944e2ba859cf26d
guodong94 <940470259@qq.com>
2018-12-11 00:09:00 +0800

    添加Rcp依赖

---
+++ api/src/main/java/com/gd/api/ProductRPC.java
@@ -0,0 +1,14 @@
+package com.gd.api;
+
+import com.googlecode.jsonrpc4j.JsonRpcService;
+
+/**
+ * @auther guodong
+ * @email https://github.com/guodong94
+ * @date 2018/12/10 23:56
+ * 产品相关的Rpc服务
+ */
+@JsonRpcService("/product")
+public interface ProductRPC {
+
+}
